dc0c42734579b8296be7a3dfec576ffabb02d457
Liudmyla <87442018+Luydmyla@users.noreply.github.com>
2023-10-11 01:16:46 +0300

    added slider to hero

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 // console.log("Hello, World!");
 // const logotype = document.querySelector(".");
 const slider = document.querySelector(".hero-list");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
-new Siema({
-  selector: ".siema",
-  duration: 200,
-  easing: "ease-out",
-  perPage: 1,
-  startIndex: 0,
-  draggable: true,
-  multipleDrag: true,
-  threshold: 20,
-  loop: false,
-  rtl: false,
-  onInit: () => {},
-  onChange: () => {},
+// new Siema({
+//   selector: ".hero",
+// });
+
+const mySiema = new Siema({
+  selector: slider,
+  duration: 1000,
+  easing: "cubic-bezier(.17,.67,.32,1.34)",
+  //   perPage: 1,
+  //   startIndex: 0,
+  //   draggable: true,
+  //   multipleDrag: true,
+  //   threshold: 20,
+  loop: true,
+  //   rtl: false,
+  //   onInit: () => {},
+  //   onChange: () => {},
 });
+
+function onClickNext() {
+  mySiema.next();
+}
+function onClickPrev() {
+  mySiema.prev();
+}
+
+next.addEventListener("click", onClickNext);
+prev.addEventListener("click", onClickPrev);
